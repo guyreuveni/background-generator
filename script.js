d12b288733eff72e2d9301ea63664c6c5b262758
guy reuveni <guyreuveni98@gmail.com>
2021-02-01 15:26:18 +0200

    little fix

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var css = document.querySelector("h3")
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.querySelector("body");
-var button = document.querySelector("button")
+var button = document.querySelector("button");
 
 function updateBackground() {
     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
